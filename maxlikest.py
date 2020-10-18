9ec6238930043090a34496639c96274722bddf92
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2020-10-18 17:58:19 +0200

    MLE

---
+++ maxlikest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 17:57:38 2020
+
+@author: montanelli
+"""
---
+++ test_maxlikest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 17:56:46 2020
+
+@author: montanelli
+"""
