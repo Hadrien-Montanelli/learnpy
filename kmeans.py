3769f02cc8c6cfb994025490ca3bed1c3f0091e3
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2020-10-23 17:33:54 +0200

    Add a kmeans feature

---
+++ kmeans.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 16:05:24 2020
+
+@author: montanelli
+"""
+import numpy as np
+from numpy import linalg as LA
+
+def kmeans(data, k):
+    """Return k clusters of data using the k-means algorithm."""
+    # Get dimension:
+    number_rows = len(data)
+    
+    # Assign each observation to one of the clusters:
+    cluster_index = []
+    for i in range(k-1):
+        cluster_index.append([i])
+    cluster_index.append([i for i in range(k-1, number_rows)])
+    
+    # Iterate the following 2 steps until cluster assignments stop changing:
+    test = 0
+    while test == 0:
+        
+        # For each cluster compute the cluster mean:
+        cluster_mean = []
+        for i in range(k):
+            cluster_mean.append(compute_cluster_mean(data, cluster_index[i]))
+            
+        # Re-assign all observations to the cluster whose mean is closest:
+        new_cluster_index = [[] for _ in range(k)]
+        for i in range(number_rows):
+            dist_to_cluster = np.zeros([k, 1])
+            for j in range(k):
+                dist_to_cluster[j] = LA.norm(data[i,:] - cluster_mean[j])**2  
+            pos_min = np.argmin(dist_to_cluster)
+            new_cluster_index[pos_min].append(i)
+
+        # Stop if clusters haven't changed:
+        if new_cluster_index == cluster_index:
+            test = 1
+        else:
+            cluster_index = new_cluster_index.copy()
+        
+    return cluster_index
+
+def compute_cluster_mean(data, cluster_index):
+    number_rows = len(cluster_index)
+    cluster_mean = 1/number_rows*np.sum(data[cluster_index, :], 0)
+    return cluster_mean
---
+++ test_kmeans.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 16:06:00 2020
+
+@author: montanelli
+"""
+from kmeans import kmeans
+from csv_to_array import csv_to_array
+import matplotlib.pyplot as plt
+
+data = csv_to_array('heights_weights_training.csv')
+clusters = kmeans(data[:,0:2], 2)
+print(clusters)
+number_rows = len(data)
+for i in range(number_rows):
+    if i in clusters[0]:
+        color = '.r'
+    else:
+        color = '.b'
+    plt.plot(data[i,0],data[i,1],color)
