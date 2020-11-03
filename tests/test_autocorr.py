424543e918c737af7fc7731c779710bbec7b7598
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2020-11-03 11:32:41 +0100

    Add comparisons with STATSMODELS in tests for AUTOCORR/PAUTOCORR

--- tests/test_autocorr.py
+++ tests/test_autocorr.py
@@ -9,16 +9,26 @@ Copyright 2020 by Hadrien Montanelli.
 import sys
 sys.path.append('../misc')
 from utilities import csv_to_array
+import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import acf
 from autocorr import autocorr
 
 # AR(1)-type time series:
 series = csv_to_array('../dataset/time_series_ar1.csv')
 sample_acf = autocorr(series)
-plt.plot(sample_acf[1:], '.-')
+plt.plot(sample_acf[1:], '-')
+sample_acf_statsmodels = acf(series, nlags=len(series), fft=False)
+plt.plot(sample_acf_statsmodels[1:], '--')
+error = np.linalg.norm(sample_acf - sample_acf_statsmodels)
+print('Error: ', error) # compare with statsmodels' acf
 
 # MA(1)-type time series:
 series = csv_to_array('../dataset/time_series_ma1.csv')
 sample_acf = autocorr(series)
 plt.figure()
-plt.plot(sample_acf[1:], '.-')
+plt.plot(sample_acf[1:], '-')
+sample_acf_statsmodels = acf(series, nlags=len(series), fft=False)
+plt.plot(sample_acf_statsmodels[1:], '--')
+error = np.linalg.norm(sample_acf - sample_acf_statsmodels)
+print('Error: ', error) # compare with statsmodels' acf
